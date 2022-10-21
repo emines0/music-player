4814d8b8e2617042c64d249f4006a006865cd67b
emines0 <emin.sefa1@gmail.com>
2022-10-21 15:36:15 +0200

    adding play, prev, next functionality

---
+++ script.js
@@ -0,0 +1,93 @@
+const image = document.querySelector('img');
+const title = document.getElementById('title');
+const artist = document.getElementById('artist');
+const music = document.querySelector('audio');
+const prevBtn = document.getElementById('prev');
+const playBtn = document.getElementById('play');
+const nextBtn = document.getElementById('next');
+
+//Music
+const songs = [
+    {
+        name: 'jacinto-1',
+        displayName: 'Electric Chill Machine',
+        artist: 'Jacinto Design'
+    },
+    {
+        name: 'jacinto-2',
+        displayName: 'Seven Nation Army (Remix)',
+        artist: 'Jacinto Design'
+    },
+    {
+        name: 'jacinto-3',
+        displayName: 'Goodnight, Disco Queen',
+        artist: 'Jacinto Design'
+    },
+    {
+        name: 'metric-1',
+        displayName: 'Front Row (Remix)',
+        artist: 'Metrics/Jacinto Design'
+    },
+];
+
+//Check if Playing
+let isPlaying = false;
+
+// Play
+
+function playSong() {
+    music.play();
+    playBtn.classList.replace('fa-play','fa-pause');
+    playBtn.setAttribute('title', 'Pause')
+    isPlaying = true;
+}
+
+function pauseSong() {
+    music.pause();
+    playBtn.classList.replace('fa-pause', 'fa-play');
+    playBtn.setAttribute('title', 'Play')
+    isPlaying = false;
+}
+
+//Current Song
+let songIndex = 0;
+
+// Prev Song
+function prevSong() {
+    songIndex--;
+
+    if(songIndex < 0) {
+        songIndex = songs.length - 1;
+    }
+
+    loadSong(songs[songIndex]);
+    playSong();
+}
+
+// Next Song
+function nextSong() {
+    songIndex++;
+
+    if(songIndex > songs.length - 1) {
+        songIndex = 0;
+    }
+
+    loadSong(songs[songIndex]);
+    playSong();
+}
+
+//Update DOM 
+function loadSong(song) {
+    title.textContent = song.displayName;
+    artist.textContent = song.artist;
+    music.src = `./music/${song.name}.mp3`;
+    image.src = `./img/${song.name}.jpg`;
+}
+
+//Event Listeners
+playBtn.addEventListener('click', () => (isPlaying ? pauseSong() : playSong() ));
+prevBtn.addEventListener('click', prevSong);
+nextBtn.addEventListener('click', nextSong);
+
+// On Load - select first song
+loadSong(songs[songIndex]);
