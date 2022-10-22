11b37a3d4924af1cda148c67ddce1be5c12b7ff1
emines0 <emin.sefa1@gmail.com>
2022-10-22 15:32:45 +0200

    functionality for moving song after click to progress bar

--- script.js
+++ script.js
@@ -3,6 +3,8 @@ const title = document.getElementById('title');
 const artist = document.getElementById('artist');
 const music = document.querySelector('audio');
 const progressContainer = document.getElementById('progress-container');
+const currentTimeEl = document.getElementById('current-time');
+const durationEl = document.getElementById('duration')
 const progress = document.getElementById('progress');
 const prevBtn = document.getElementById('prev');
 const playBtn = document.getElementById('play');
@@ -81,8 +83,8 @@ function nextSong() {
 
 //Update DOM 
 function loadSong(song) {
-    title.textContent = song.displayName;
-    artist.textContent = song.artist;
+    title.textContent = song.displayName; //in this case is better to have textContent ont innerHTML because innerHTML recomputed value each time
+    artist.textContent = song.artist;     // if we have same value textContent doesn't recompute it
     music.src = `./music/${song.name}.mp3`;
     image.src = `./img/${song.name}.jpg`;
 }
@@ -94,7 +96,36 @@ function updateProgressBar(e) {
         // Update progress bar width 
         const progressPercent = (currentTime / duration) * 100;
         progress.style.width = `${progressPercent}%`;
+        // Calculate display for duration
+        const durationMinutes = Math.floor(duration / 60);
+        let durationSeconds = Math.floor(duration % 60); //Reminder
+
+        if(durationSeconds<10) {
+            durationSeconds = `0${durationSeconds}`;
+        }
+
+        // Delay switching duration ELement to avoid NaN
+        if(durationSeconds){
+            durationEl.textContent = `${durationMinutes}:${durationSeconds}`;
+        }
+
+        // Calculate display for current
+        const currentMinutes = Math.floor(currentTime / 60);
+        let currentSeconds = Math.floor(currentTime % 60); //Reminder
+
+        if(currentSeconds<10) {
+            currentSeconds = `0${currentSeconds}`;
+        }
+
+        currentTimeEl.textContent = `${currentMinutes}:${currentSeconds}`;
     }
+}
+
+function setProgressBar(e) {
+    const width = this.clientWidth; // width of progresbarr
+    const clickX = e.offsetX; // where was clicked
+    const {duration} = music;
+    music.currentTime = (clickX/width) * duration; // set certain seconds of the song where was clicked
 }
 
 //Event Listeners
@@ -102,6 +133,8 @@ playBtn.addEventListener('click', () => (isPlaying ? pauseSong() : playSong() ))
 prevBtn.addEventListener('click', prevSong);
 nextBtn.addEventListener('click', nextSong);
 music.addEventListener('timeupdate', updateProgressBar);
+progressContainer.addEventListener('click', setProgressBar);
+music.addEventListener('ended', nextSong); // at the end of the song automatically switch to the next song 
 
 // On Load - select first song
 loadSong(songs[songIndex]);
